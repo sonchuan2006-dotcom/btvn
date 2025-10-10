de6da50770e2d8e69efb1341afe2d99a6aff59e2
Nguyen Dinh Son <sonchuan2006@gmail.com>
2025-10-10 14:30:57 +0700

    Update btvn.c

--- btvn.c
+++ btvn.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
-    int a=27;
-    int b=2;
+    int a=30;
+    int b=19;
     int c=27*2;
     for(int i=0;i<=6;i++){
         c+=i;
     }
     printf("%d",c);
+
 }
